6ee9887e5fe1ef3b1d6a53788a1f0701f8728acf
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 11:06:23 +0200

    EmptySetException JavaDoc + rivista NoValueException javadoc

--- KmeansServer/src/database/EmptySetException.java
+++ KmeansServer/src/database/EmptySetException.java
@@ -1,5 +1,12 @@
 package database;
 
+/**
+ * EmptySetException è la classe che gestisce le eccezioni lanciate in caso di risultati vuoti da una query.
+ * <p>
+ * Questa eccezione viene lanciata quando il risultato di una qeury è vuoto.
+ * Viene considerato un errore in quanto non è possibile eseguire operazioni su un risultato vuoto.
+ * @see Exception
+ */
 public class EmptySetException extends Exception {
     EmptySetException(String msg) {
         super(msg);
--- KmeansServer/src/database/NoValueException.java
+++ KmeansServer/src/database/NoValueException.java
@@ -1,8 +1,10 @@
 package database;
 
 /**
- * NoValueException è la classe che gestisce le eccezioni lanciate in caso
- * risultati vuoti da una query.
+ * NoValueException è la classe che gestisce le eccezioni lanciate in caso di risultati vuoti da una query di aggregazione.
+ * <p>
+ * Questa eccezione viene lanciata quando il risultato di una qeury di aggregazione è vuoto.
+ * Viene considerato un errore in quanto non è possibile eseguire operazioni su un risultato vuoto.
  * @see Exception
  */
 public class NoValueException extends Exception {
