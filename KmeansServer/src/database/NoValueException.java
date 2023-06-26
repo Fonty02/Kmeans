fa6b386e5caaf5e7e3cd30c9c65c65dfc997a5bc
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 10:47:48 +0200

    Aggiunta JavaDoc per NoValueException

--- KmeansServer/src/database/NoValueException.java
+++ KmeansServer/src/database/NoValueException.java
@@ -1,6 +1,16 @@
 package database;
 
+/**
+ * NoValueException è la classe che gestisce le eccezioni lanciate in caso
+ * risultati vuoti da una query.
+ * @see Exception
+ */
 public class NoValueException extends Exception {
+
+    /**
+     * Costruttore della classe NoValueException.
+     * @param msg messaggio di errore
+     */
     NoValueException(String msg) {
         super(msg);
     }
