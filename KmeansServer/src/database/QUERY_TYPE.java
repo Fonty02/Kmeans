b3a478a81870bce4071558fdcb7b4b528af17784
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 10:49:24 +0200

    JavaDoc per QUERY_TYPE

--- KmeansServer/src/database/QUERY_TYPE.java
+++ KmeansServer/src/database/QUERY_TYPE.java
@@ -1,5 +1,12 @@
 package database;
 
+/**
+ * QUERY_TYPE è l'enumerazione che definisce i tipi di query che possono essere effettuate.
+ */
 public enum QUERY_TYPE {
+    /**
+     * MIN indica che la query da effettuare è di tipo MIN.
+     * MAX indica che la query da effettuare è di tipo MAX.
+     */
     MIN, MAX
 }
