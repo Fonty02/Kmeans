bfd0939448384ffe5d84bf9f829999ed4d549ca8
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 10:41:40 +0200

    JavaDoc per DatabaseConnectionException

--- KmeansServer/src/database/DatabaseConnectionException.java
+++ KmeansServer/src/database/DatabaseConnectionException.java
@@ -1,6 +1,16 @@
 package database;
 
+/**
+ * DatabaseConnectionException è la classe che gestisce le eccezioni lanciate in caso di problemi
+ * relativi alla comunicazione con il database.
+ * @see Exception
+ */
 public class DatabaseConnectionException extends Exception {
+
+    /**
+     * Costruttore della classe DatabaseConnectionException.
+     * @param msg messaggio di errore
+     */
     DatabaseConnectionException(String msg) {
         super(msg);
     }
