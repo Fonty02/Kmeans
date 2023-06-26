ab1a6c9f1ffde94ff60006deaf9958d2bde570a6
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 12:17:23 +0200

    JavaDoc per DiscreteItem

--- KmeansServer/src/data/DiscreteItem.java
+++ KmeansServer/src/data/DiscreteItem.java
@@ -1,11 +1,25 @@
 package data;
 
+/**
+ * La classe DiscreteItem rappresenta un item di tipo discreto di un dataset.
+ * Un item discreto è una coppia (attributo discreto, valore discreto).
+ */
 class DiscreteItem extends Item {
+
+    /**
+     * Costruttore della classe DiscreteItem.
+     * @param attribute Attributo discreto dell'item.
+     * @param value Valore discreto dell'item.
+     */
     DiscreteItem(DiscreteAttribute attribute, String value) {
         super(attribute, value);
     }
 
-    //distanza 1 se oggetti diversi (stringhe nel caso), 0 se uguali
+    /**
+     * Restituisce la distanza tra l'item e l'oggetto passato come parametro.
+     * @param a oggetto di cui calcolare la distanza
+     * @return distanza tra l'item e l'oggetto passato come parametro
+     */
     double distance(Object a) {
         return getValue().equals(a) ? 0 : 1;
     }
