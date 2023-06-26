5e5f0368003c1a7616016b670f3368dfaba6343d
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 11:14:45 +0200

    JavaDoc per TableSchema

--- KmeansServer/src/database/TableSchema.java
+++ KmeansServer/src/database/TableSchema.java
@@ -8,38 +8,81 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * La classe TableSchema permette di ottenere lo schema di una tabella del database relazionale.
+ */
 public class TableSchema {
+
+    /**
+     * Riferimento al database.
+     */
     DbAccess db;
 
+    /**
+     * La classe Column rappresenta una colonna della tabella.
+     * Una colonna viene rappresentata mediante il nome e il tipo di dato.
+     */
     public class Column {
+
+        /**
+         * Nome della colonna.
+         */
         private final String name;
+        /**
+         * Tipo di dato della colonna.
+         */
         private final String type;
 
+        /**
+         * Costruttore della classe Column.
+         * @param name Nome della colonna.
+         * @param type Tipo di dato della colonna.
+         */
         private Column(String name, String type) {
             this.name = name;
             this.type = type;
         }
 
+        /**
+         * Restituisce il nome della colonna.
+         * @return Nome della colonna.
+         */
         public String getColumnName() {
             return name;
         }
 
+        /**
+         * Restituisce il tipo di dato della colonna.
+         * @return Tipo di dato della colonna.
+         */
         public boolean isNumber() {
             return type.equals("number");
         }
 
+        /**
+         * Restituisce la stringa che rappresenta la colonna.
+         * @return Stringa che rappresenta la colonna.
+         */
         public String toString() {
             return name + ":" + type;
         }
 
     }
 
+    /**
+     * Lista delle colonne della tabella.
+     */
     private List<Column> tableSchema = new ArrayList<>();
 
+    /**
+     * Costruttore della classe TableSchema.
+     * @param db Riferimento al database.
+     * @param tableName Nome della tabella.
+     * @throws SQLException Eccezione lanciata in caso di errore nell'esecuzione della query.
+     */
     public TableSchema(DbAccess db, String tableName) throws SQLException {
         this.db = db;
         HashMap<String, String> mapSQL_JAVATypes = new HashMap<>();
-        //http://java.sun.com/j2se/1.3/docs/guide/jdbc/getstart/mapping.html
         mapSQL_JAVATypes.put("CHAR", "string");
         mapSQL_JAVATypes.put("VARCHAR", "string");
         mapSQL_JAVATypes.put("LONGVARCHAR", "string");
@@ -64,10 +107,19 @@ public class TableSchema {
         res.close();
     }
 
+    /**
+     * Restituisce il numero di colonne della tabella.
+     * @return Numero di colonne della tabella.
+     */
     public int getNumberOfAttributes() {
         return tableSchema.size();
     }
 
+    /**
+     * Restituisce la colonna della tabella all'indice specificato.
+     * @param index Indice della colonna.
+     * @return Colonna della tabella all'indice specificato.
+     */
     public Column getColumn(int index) {
         return tableSchema.get(index);
     }
