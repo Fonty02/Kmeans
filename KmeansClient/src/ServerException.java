63006b46b4b1cc1c485f2841e8414c72de63c90d
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 09:07:42 +0200

    JavaDoc di ServerException

--- KmeansClient/src/ServerException.java
+++ KmeansClient/src/ServerException.java
@@ -1,4 +1,12 @@
- class ServerException extends Exception{
+/**
+ * ServerException è la classe che gestisce le eccezioni che possono essere lanciate dal server.
+ * @see Exception
+ */
+class ServerException extends Exception{
+    /**
+     * Costruttore della classe ServerException.
+     * @param msg messaggio di errore.
+     */
     ServerException(String msg)
     {
         super(msg);
