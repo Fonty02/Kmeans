11f1de622cff8b88ecaf991437ffb160652b4ef7
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 12:16:44 +0200

    JavaDoc per ContinuousItem

--- KmeansServer/src/data/ContinuousItem.java
+++ KmeansServer/src/data/ContinuousItem.java
@@ -1,10 +1,25 @@
 package data;
 
+/**
+ * La classe ContinuousItem rappresenta un item di tipo continuo di un dataset.
+ * Un item continuo è una coppia (attributo continuo, valore continuo).
+ */
 class ContinuousItem extends Item {
+
+    /**
+     * Costruttore della classe ContinuousItem.
+     * @param attribute Attributo continuo dell'item.
+     * @param value Valore continuo dell'item.
+     */
     ContinuousItem(Attribute attribute, Double value) {
         super(attribute, value);
     }
 
+    /**
+     * Restituisce la distanza tra l'item e l'oggetto passato come parametro.
+     * @param a oggetto di cui calcolare la distanza
+     * @return distanza tra l'item e l'oggetto passato come parametro
+     */
     double distance(Object a) {
         return Math.abs(((ContinuousAttribute) this.attribute).getScaledValue((Double) value) - ((ContinuousAttribute) this.attribute).getScaledValue((Double) a));
     }
