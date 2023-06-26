c3e67bdb58395110702bbaf0e45d3bb3791150f9
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 09:57:34 +0200

    Aggiunto costruttore privato Keyboard

--- KmeansClient/src/keyboardinput/Keyboard.java
+++ KmeansClient/src/keyboardinput/Keyboard.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- * Keybpard è la classe che gestisce l'input da tastiera.
+ * Keyboard è la classe che gestisce l'input da tastiera.
  * In particolare la classe permette di leggere le seguenti tipologie di input
  * <ul>
  *     <li>Stringhe</li>
@@ -15,53 +15,60 @@ import java.io.InputStreamReader;
 public class Keyboard {
 
 	/**
-	 * in è l'oggetto che permette di leggere l'input da tastiera.
-	 * @see BufferedReader
+	 * Costruttore privato per evitare che venga istanziata la classe.
 	 */
-	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-	/**
-	 * readString è il metodo che permette di leggere una stringa da tastiera.
-	 * <p>
-	 * In caso di errore nella lettura dell'input viene restituito il valore di default e viene stampato un messaggio di errore.
-	 * @return la <code>stringa</code> letta da tastiera.
-	 * </p>
-	 */
-	public static String readString() {
-		String str;
-		try {
-			str = in.readLine();
-		} catch (Exception exception) {
-			System.err.println("ERRORE NELLA LETTURA DELLA STRINGA");
-			str = null;
-		}
-		return str;
+	private Keyboard() {
 	}
 
 	/**
-	 * readInt è il metodo che permette di leggere un intero da tastiera.
-	 * <p>
-	 * Nel caso in cui la conversione a intero non avviene con successo viene stampato un messaggio di errore e
-	 * viene chiesto di reinserire il valore.
-	 * Nel caso in cui la lettura dell'input da tastiera non avviene con successo viene restituito il valore di default e viene stampato un messaggio di errore.
-	 * @return l'<code>intero</code> letto da tastiera.
-	 * </p>
-	 */
+     * in è l'oggetto che permette di leggere l'input da tastiera.
+     *
+     * @see BufferedReader
+     */
+    private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-	public static int readInt() {
-		int value = 0;
-		String str;
-		while(true) {
-			try {
-				str = in.readLine();
-				value = Integer.parseInt(str);
-				return value;
-			} catch (NumberFormatException e) {
-				System.out.println("LETTO VALORE NON INTERO, REINSERIRE");
-			} catch (Exception exception) {
-				System.err.println("Errore nella lettura del numero. Restituito -1 di default");
-				return -1;
-			}
-		}
-	}
+    /**
+     * readString è il metodo che permette di leggere una stringa da tastiera.
+     * <p>
+     * In caso di errore nella lettura dell'input viene restituito il valore di default e viene stampato un messaggio di errore.
+     *
+     * @return la <code>stringa</code> letta da tastiera.
+     */
+    public static String readString() {
+        String str;
+        try {
+            str = in.readLine();
+        } catch (Exception exception) {
+            System.err.println("ERRORE NELLA LETTURA DELLA STRINGA");
+            str = null;
+        }
+        return str;
+    }
+
+    /**
+     * readInt è il metodo che permette di leggere un intero da tastiera.
+     * <p>
+     * Nel caso in cui la conversione a intero non avviene con successo viene stampato un messaggio di errore e
+     * viene chiesto di reinserire il valore.
+     * Nel caso in cui la lettura dell'input da tastiera non avviene con successo viene restituito il valore di default e viene stampato un messaggio di errore.
+     *
+     * @return l'<code>intero</code> letto da tastiera.
+     */
+
+    public static int readInt() {
+        int value = 0;
+        String str;
+        while (true) {
+            try {
+                str = in.readLine();
+                value = Integer.parseInt(str);
+                return value;
+            } catch (NumberFormatException e) {
+                System.out.println("LETTO VALORE NON INTERO, REINSERIRE");
+            } catch (Exception exception) {
+                System.err.println("Errore nella lettura del numero. Restituito -1 di default");
+                return -1;
+            }
+        }
+    }
 }
