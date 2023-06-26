5b1a973e3eec557f57f91e832ecdebb16fe05619
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 11:27:26 +0200

    JavaDoc per Example

--- KmeansServer/src/database/Example.java
+++ KmeansServer/src/database/Example.java
@@ -3,17 +3,40 @@ package database;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La classe Example rappresenta una transazione del database.
+ * Una transazione viene rappresentata mediante una lista di oggetti.
+ */
 public class Example implements Comparable<Example> {
+
+    /**
+     * Lista di oggetti che rappresentano una transazione.
+     */
     private final List<Object> example = new ArrayList<>();
 
+    /**
+     * Aggiunge un oggetto alla transazione.
+     * @param o Oggetto da aggiungere alla transazione.
+     */
     void add(Object o) {
         example.add(o);
     }
 
+    /**
+     * Restituisce l'ogetto in posizione i.
+     * @param i Posizione dell'oggetto da restituire.
+     * @return Oggetto in posizione i.
+     */
     public Object get(int i) {
         return example.get(i);
     }
 
+    /**
+     * compareTo confronta due transazioni.
+     * Due transizioni sono uguali se hanno gli stessi oggetti nella stessa posizione.
+     * @param ex la transazione da confrontare.
+     * @return 0 se le transizioni sono uguali, un altro intero se sono diverse.
+     */
     public int compareTo(Example ex) {
         int i = 0;
         for (Object o : ex.example) {
@@ -23,6 +46,10 @@ public class Example implements Comparable<Example> {
         return 0;
     }
 
+    /**
+     * Restituisce la stringa che rappresenta la transazione.
+     * @return Stringa che rappresenta la transazione.
+     */
     public String toString() {
         String str = "";
         for (Object o : example)
