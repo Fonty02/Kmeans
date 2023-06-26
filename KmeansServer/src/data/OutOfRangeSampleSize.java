125dd680759369d2af807aae25e437ce7410e68c
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 11:51:30 +0200

    JavaDoc per OutOfRangeSampleSize

--- KmeansServer/src/data/OutOfRangeSampleSize.java
+++ KmeansServer/src/data/OutOfRangeSampleSize.java
@@ -1,6 +1,14 @@
 package data;
 
+/**
+ * Eccezione che viene lanciata quando la dimensione del sample è fuori dal range consentito.
+ */
 public class OutOfRangeSampleSize extends Exception {
+
+    /**
+     * Costruttore della classe.
+     * @param msg messaggio di errore
+     */
     public OutOfRangeSampleSize(String msg) {
         super(msg);
     }
