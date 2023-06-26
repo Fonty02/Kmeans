04273589040da9e8f7964c2d33fe7d6599c9e945
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-06-26 11:50:59 +0200

    JavaDoc per DiscreteAttribute

--- KmeansServer/src/data/DiscreteAttribute.java
+++ KmeansServer/src/data/DiscreteAttribute.java
@@ -5,20 +5,47 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ * Classe che rappresenta un attributo discreto (stringa).
+ * <p>
+ * Estende la classe astratta {@link Attribute} e implementa l'interfaccia {@link Iterable} per poter iterare sui valori del dominio dell'attributo.
+ */
 class DiscreteAttribute extends Attribute implements Iterable<String> {
-    private final TreeSet<String> values; //Possibili valori del dominio dell'attributo. ES: per playTennis abbiamo yes o no
 
+    /**
+     * Insieme ordinato dei valori del dominio dell'attributo.
+     */
+    private final TreeSet<String> values;
+
+    /**
+     * Costruttore della classe.
+     *
+     * @param name   nome dell'attributo
+     * @param index  indice dell'attributo
+     * @param values array di stringhe che rappresentano i valori del dominio dell'attributo
+     */
     DiscreteAttribute(String name, int index, String[] values) {
         super(name, index);
         this.values = new TreeSet<>();
         Collections.addAll(this.values, values);
     }
 
+    /**
+     * Restituisce il numero di valori del dominio dell'attributo.
+     *
+     * @return numero di valori del dominio dell'attributo
+     */
     public Iterator<String> iterator() {
         return this.values.iterator();
     }
 
-    //restituisce quante volte v compare nel dataset
+    /**
+     * Restituisce il numero di volte che il valore v compare nel dataset.
+     * @param data dataset
+     * @param idList l'insieme degli indici di riga
+     * @param v valore
+     * @return numero di volte che il valore v compare nel dataset
+     */
     int frequency(Data data, Set<Integer> idList, String v) {
         int count = 0;
         for (int i : idList) {
